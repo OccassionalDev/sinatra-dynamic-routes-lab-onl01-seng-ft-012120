237ec22ac56574cc0bf0ee2c1bb65df52bcb4c4a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-03-06 02:12:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end 
   
   get '/square/:number' do 
-    params[:number]*params[:number]
+    (params[:number].to_i * params[:number].to_i).to_s
   end 
   
   get '/say/:number/:phrase' do 
